eec9553e8cf8d7e87e3eb33b1db9df32551651b7
Jawaheryslam <orchidgraphics19@gmail.com>
2026-01-05 22:44:21 +0300

    Phase 2: Add Goat REST API with Django REST Framework

--- goats/views.py
+++ goats/views.py
@@ -1,3 +1,8 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from .models import Goat
+from .serializers import GoatSerializer
 
-# Create your views here.
+
+class GoatViewSet(viewsets.ModelViewSet):
+    queryset = Goat.objects.all()
+    serializer_class = GoatSerializer
