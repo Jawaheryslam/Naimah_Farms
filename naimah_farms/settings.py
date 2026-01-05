23422d17a0ddb59a76c210314c79a83c49afc883
Jawaheryslam <orchidgraphics19@gmail.com>
2026-01-05 23:46:23 +0300

    Phase 3: Switch database from SQLite to PostgreSQL

--- naimah_farms/settings.py
+++ naimah_farms/settings.py
@@ -76,8 +76,12 @@ WSGI_APPLICATION = 'naimah_farms.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'naimah_farms_db',
+        'USER': 'naimah_user',
+        'PASSWORD': 'naima63',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
